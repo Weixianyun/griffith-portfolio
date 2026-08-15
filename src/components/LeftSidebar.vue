780b1e25714f187824c5e885e00eb336bdb7faf7
Gr1ff1th <weixianyun55@gmail.com>
2026-08-16 06:59:18 +0800

    init: components

---
+++ src/components/LeftSidebar.vue
@@ -0,0 +1,23 @@
+<script setup>
+import ProfileCard from './ProfileCard.vue'
+import NoticeCard from './NoticeCard.vue'
+import TagCloud from './TagCloud.vue'
+import EagleDecoration from './EagleDecoration.vue'
+</script>
+
+<template>
+  <div class="left-sidebar">
+    <ProfileCard />
+    <NoticeCard />
+    <TagCloud />
+    <EagleDecoration />
+  </div>
+</template>
+
+<style scoped>
+.left-sidebar {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+</style>
