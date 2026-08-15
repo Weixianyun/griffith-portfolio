58514ce9557d9d297310033dff169442efd153ba
Gr1ff1th <weixianyun55@gmail.com>
2026-08-16 06:58:27 +0800

    init: components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { SITE } from '../data/site'
+</script>
+
+<template>
+  <footer class="app-footer">
+    <div class="footer-inner">
+      <p>{{ SITE.copyright }}</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.app-footer {
+  margin-top: 40px;
+  padding: 24px 16px;
+  text-align: center;
+  color: var(--color-text-muted);
+  font-size: 13px;
+  border-top: 1px solid var(--color-border);
+}
+</style>
