29c606af80368c29423a08640edb7fb5c20ced04
Gr1ff1th <weixianyun55@gmail.com>
2026-08-16 07:00:28 +0800

    init: views

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import HeroSection from '../components/HeroSection.vue'
+import ProjectsSection from '../components/ProjectsSection.vue'
+import ArticlesSection from '../components/ArticlesSection.vue'
+import TechStackSection from '../components/TechStackSection.vue'
+</script>
+
+<template>
+  <div class="home">
+    <HeroSection />
+    <ProjectsSection :limit="4" />
+    <ArticlesSection />
+    <TechStackSection />
+  </div>
+</template>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+</style>
