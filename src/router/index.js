1570174e1c6aa39a637ce6e37e4c3634e537f9e1
Gr1ff1th <weixianyun55@gmail.com>
2026-08-16 06:57:35 +0800

    init: router

---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/HomeView.vue'),
+    meta: { title: '首页' }
+  },
+  {
+    path: '/projects',
+    name: 'projects',
+    component: () => import('../views/ProjectsView.vue'),
+    meta: { title: '项目展示' }
+  },
+  {
+    path: '/diary',
+    name: 'diary',
+    component: () => import('../views/DiaryView.vue'),
+    meta: { title: '日记' }
+  },
+  {
+    path: '/album',
+    name: 'album',
+    component: () => import('../views/AlbumView.vue'),
+    meta: { title: '相册' }
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue'),
+    meta: { title: '关于' }
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+  scrollBehavior() {
+    return { top: 0 }
+  }
+})
+
+router.afterEach((to) => {
+  document.title = `${to.meta.title || '鷹の団'} · 鷹の団`
+})
+
+export default router
